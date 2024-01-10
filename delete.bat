6ec232f84445b0fa44b6bf79aaf080d6bf8b78f6
hellonguyenhoang <115633226+hellonguyenhoang@users.noreply.github.com>
2024-01-10 12:40:30 +0700

    Add files via upload

---
+++ delete.bat
@@ -0,0 +1,3 @@
+powershell -Command "Remove-Item '%appdata%\Macromedia\*' -Recurse -Force"
+powershell -Command "Remove-Item '%appdata%\Adobe\Flash Player\*' -Recurse -Force"
+powershell -Command "Remove-Item 'logdebug\*' -Recurse -Force"
